fb53447ee3ab01fa305898fecbab9f6434875d39
10500007 <85374439+10500007@users.noreply.github.com>
2021-09-28 20:03:48 -0300

    antes

--- Projeto 1/Desenv - Youtube site com bootstrap/js/codigo.js
+++ Projeto 1/Desenv - Youtube site com bootstrap/js/codigo.js
@@ -11,19 +11,9 @@ jQuery(document).ready(function($){
 
     $('.owl-carousel').owlCarousel({
         loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
+        margin:10,
+        nav:true
+    
         
     })
 
