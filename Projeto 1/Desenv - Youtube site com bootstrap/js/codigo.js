cd4d5958d83c0a5d61fc2fb8f8cdddce92a6b9c3
10500007 <felipe_augusto.bn@hotmail.com>
2021-09-28 12:59:52 -0300

    att

--- Projeto 1/Desenv - Youtube site com bootstrap/js/codigo.js
+++ Projeto 1/Desenv - Youtube site com bootstrap/js/codigo.js
@@ -11,6 +11,19 @@ jQuery(document).ready(function($){
 
     $('.owl-carousel').owlCarousel({
         loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
         
     })
 
